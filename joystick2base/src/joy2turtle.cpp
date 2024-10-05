bcadfa284ec1b4600221c8fa5a6906f4df14623b
fire <lord.daniel.w@gmail.com>
2024-10-04 20:00:40 -0400

    Placed private members above methods and added to do

--- joystick2base/src/joy2turtle.cpp
+++ joystick2base/src/joy2turtle.cpp
@@ -8,6 +8,7 @@ class Joy2Turtle : public rclcpp::Node
 public:
   Joy2Turtle() : Node("joy2turtle"), l_scale_(2.0), a_scale_(2.0)
   {
+    // To Do : Add parameters for topic turtle1 and linear and angular scale
     twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
     joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
       "joy", 10, std::bind(&Joy2Turtle::joyCallback, this, std::placeholders::_1));
@@ -15,6 +16,10 @@ public:
   }
 
 private:
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
+  double l_scale_, a_scale_;
+
   void joyCallback(const sensor_msgs::msg::Joy::SharedPtr joy_msg)
   {
     geometry_msgs::msg::Twist twist;
@@ -27,10 +32,6 @@ private:
 
     twist_pub_->publish(twist);
   }
-
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
-  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
-  double l_scale_, a_scale_;
 };
 
 int main(int argc, char** argv)
