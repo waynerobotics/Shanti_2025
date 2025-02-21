430b10aed6d92d88ed6d23bf57294baf14242ac2
LuayJ <gc9570@wayne.edu>
2025-02-21 17:36:45 -0500

    Moved urdfs from src/description to description

--- shanti_base/setup.py
+++ shanti_base/setup.py
@@ -11,7 +11,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),  # ✅ Ensures launch files are installed
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'description'), glob('description/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
