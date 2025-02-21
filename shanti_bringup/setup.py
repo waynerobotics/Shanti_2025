5bc1682bc49e23e6a93eda9857e3de39fcce65ff
LuayJ <gc9570@wayne.edu>
2025-02-21 17:36:02 -0500

    Added Shanti_bringup package

---
+++ shanti_bringup/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'shanti_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aasha',
+    maintainer_email='gc9570@wayne.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
