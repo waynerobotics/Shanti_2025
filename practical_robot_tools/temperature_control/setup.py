709949a0d1ee6b4029e1ff9b810f4598ee27af25
FireLord711 <dan.lord@boncormedical.com>
2025-05-19 15:02:55 -0400

    moved tempPackage to practical control.  Renamed Temperature Control

--- practical_robot_tools/temperature_control/setup.py
+++ practical_robot_tools/temperature_control/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'tempPackage'
+package_name = 'temperature_control'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-		'aishaPublisher1 = tempPackage.aishaPublisher1:main',
+		'aishaPublisher1 = temperature_control.aishaPublisher1:main',
         ],
     },
 )
